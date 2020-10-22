e1f8108b91c33a4bdb06d280e127ad841a6e08e7
Dom Youmans <ddyoumans@icloud.com>
2020-10-22 09:49:04 -0700

    update navigation and rename product.js

--- navigation/index.js
+++ navigation/index.js
@@ -7,3 +7,19 @@ import Explore from '../screens/Explore';
 import Login from '../screens/Login';
 import Product from '../screens/Product';
 import Settings from '../screens/Settings';
+
+import { theme } from '../constants';
+
+const screens = createStackNavigator({
+    Welcome,
+        Browse,
+        Explore,
+        Login,
+        Product,
+        Settings,
+
+}, {
+    defaultNavigationOptions: {
+        
+    }
+});
