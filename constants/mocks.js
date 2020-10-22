741fbc664aa3b4c7638f18646f72801f4ef43260
Dom Youmans <ddyoumans@icloud.com>
2020-10-22 10:49:03 -0700

    update mocks.js

--- constants/mocks.js
+++ constants/mocks.js
@@ -1,9 +1,57 @@
 const categories = [
+{
+    id: 'plants',
+    name: 'Plants',
+    tags: ['products', 'inspirations'],
+    count: 147,
+    image: require('../assets/images/plants.png')
+},
+{
+    id: 'seeds',
+    name: 'Se',
+    tags: ['products', 'shop'],
+    count: 16,
+    image: require('../assets/images/seeds.png')
+},
+{
+    id: 'flowers',
+    name: 'Flowers',
+    tags: ['products', 'inspirations'],
+    count: 68,
+    image: require('../assets/images/flowers.png')
+},
+{
+    id: 'sprayers',
+    name: 'Sprayers',
+    tags: ['products', 'inspirations'],
+    count: 68,
+    image: require('../assets/images/sprayers.png')
+},
+{
+    id: 'pots',
+    name: 'Pots',
+    tags: ['products', 'shop'],
+    count: 47,
+    image: require('../assets/images/pots.png')
+},
+{
+    id: 'fertilizers',
+    name: 'Fertilizers',
+    tags: ['products', 'shop'],
+    count: 47,
+    image: require('../assets/images/fertilizers.png')
+},
 
 ];
 
+
 const products = [
 
+
+];
+
+const explore = [
+//  images
 
 ];
 
